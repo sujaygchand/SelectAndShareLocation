2bc63c156706384101e3ed06aaaf7df272602880
Sujay Chand <sujaygchand@gmail.com>
2021-10-05 22:00:43 +1300

    Started main logic

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+import { Validation } from "./validation";
+let form : HTMLFormElement; 
+let addressInput : HTMLInputElement;
+
+function Init() {
+  form = document.querySelector("form") as HTMLFormElement;
+  Validation.checkHTMLElementIsValid([form], "form cannot be found on page");
+  
+  addressInput = document.getElementById("address") as HTMLInputElement;
+  Validation.checkHTMLElementIsValid([addressInput], "address cannot be found on page");
+}
+
+function searchAddressHandler(event: Event){
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+}
+
+// Execution Logic start
+Init();
